aa417550cb4cf2c9b1102bd9de2bf0070e4bdaa4
dovadi77 <flashshadow32@gmail.com>
2025-03-08 17:35:32 +0700

    initialize commit

---
+++ src/components/SimpleForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button";
+
+const SimpleForm = () => {
+	return (
+		<div>
+			<p>This is a form</p>
+			<Button>Submit</Button>
+		</div>
+	);
+};
+
+export default SimpleForm;
